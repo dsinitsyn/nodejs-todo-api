c28953fdf37ca8315e250e4711e40262f16a7c90
crash <dima@pindrop.nl>
2018-01-02 16:22:26 +0300

    Add GET /users/me

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 const {ObjectID} = require('mongodb');
 
@@ -19,9 +20,6 @@ app.post('/users', (req, res) => {
     let body = _.pick(req.body, ['email', 'password']);
     let user = new User(body);
 
-    //User.findByToken;
-    //user.generateAuthToken;
-
     user.save().then(() => {
         return user.generateAuthToken();
     }).then((token) => {
@@ -29,6 +27,10 @@ app.post('/users', (req, res) => {
     }).catch(e => {
         res.status(400).send(e);
     })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.post('/todos', (req, res) => {
