7603322d1a725dc66d42fd5c5e1c47a66d3fd913
crash <dima@pindrop.nl>
2018-01-03 15:59:36 +0300

    config changes

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,19 @@
 let env = process.env.NODE_ENV || 'development';
 
-if ( env === 'development' ){
-    process.env.PORT = 3002;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if ( env === 'test' ){
-    process.env.PORT = 6000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if ( env === 'development' || env === 'test' ){
+    let config = require('./config.json');
+    let configEnv = config[env];
+
+    Object.keys(configEnv).forEach((key) => {
+        process.env[key] = configEnv[key];
+    });
+
 }
+
+//if ( env === 'development' ){
+//    process.env.PORT = 3002;
+//    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+//} else if ( env === 'test' ){
+//    process.env.PORT = 6000;
+//    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+//}
